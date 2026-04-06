7a14c6905aac6572c0196cfeb1798ffd536a9f6b
benjaminDreje <benjamindreje@gmail.com>
2026-04-06 18:25:31 +0300

    Poppins  font

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,11 +2,17 @@
 import Footer from "./components/Footer/Footer";
 import NavBar from "./components/NavBar/NavBar";
 import "./globals.css";
+import { Poppins } from "next/font/google";
 
+// הגדרת הפונט
+const poppins = Poppins({ 
+  subsets: ["latin"], 
+  weight: ["400", "700"], // 400 זה רגיל, 700 זה מודגש
+});
 export default function RootLayout({ children }) {
   return (
     <html lang="en" >
-      <body>
+      <body className={poppins.className}>
         <NavBar />
         {children}
         <Footer/>
